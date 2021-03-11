1b7791030905b715adeef372f1592fe9f52f72e7
MaikeV <maike.voss7@gmail.com>
2021-03-11 16:58:10 +0100

    initial commit

---
+++ src/components/Editor.vue
@@ -0,0 +1,28 @@
+<template>
+    <editor-content :editor="editor"/>
+</template>
+
+<script>
+    import {Editor, EditorContent} from 'tiptap'
+
+    export default {
+        //name: "Editor"
+        components: {
+            EditorContent,
+        },
+        data() {
+            return {
+                editor: new Editor({
+                    content: '<p>Initial editor content</p>'
+                })
+            }
+        },
+        beforeDestroy() {
+            this.editor.destroy()
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
